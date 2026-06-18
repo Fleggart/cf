84d8b912fb5054989b39417ee65d1e4965095116
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-06-18 18:36:14 +0800

    Update BotanicAdditions.java

--- src/main/java/tk/zeitheron/botanicadds/BotanicAdditions.java
+++ src/main/java/tk/zeitheron/botanicadds/BotanicAdditions.java
@@ -4,7 +4,6 @@ import com.zeitheron.hammercore.HammerCore;
 import com.zeitheron.hammercore.internal.SimpleRegistration;
 import com.zeitheron.hammercore.utils.AnnotatedInstanceUtil;
 import com.zeitheron.hammercore.utils.ForgeRegistryUtils;
-import com.zeitheron.hammercore.utils.ReflectionUtil;
 import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
@@ -20,7 +19,6 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.ModContainer;
 import net.minecraftforge.fml.common.SidedProxy;
-import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLLoadCompleteEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -29,6 +27,10 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import tk.zeitheron.botanicadds.compat.crafttweaker.CraftTweakerCompat;
 import tk.zeitheron.botanicadds.compat.crafttweaker.core.ICTCompat;
+import tk.zeitheron.botanicadds.flowers.LightningFlower;
+import tk.zeitheron.botanicadds.flowers.RainFlower;
+import tk.zeitheron.botanicadds.flowers.SnowFlower;
+import tk.zeitheron.botanicadds.flowers.WitherFlower;
 import tk.zeitheron.botanicadds.flowers.base.Flower;
 import tk.zeitheron.botanicadds.init.*;
 import tk.zeitheron.botanicadds.proxy.CommonProxy;
@@ -39,107 +41,115 @@ import vazkii.botania.common.block.ModBlocks;
 import vazkii.botania.common.item.block.ItemBlockSpecialFlower;
 import vazkii.botania.common.lib.LibOreDict;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @EventBusSubscriber
 @Mod(modid = InfoBA.MOD_ID, name = InfoBA.MOD_NAME, version = InfoBA.MOD_VERSION, dependencies = "required-after:hammercore;required-after:botania")
 public class BotanicAdditions
 {
-	public static final List<Class<? extends SubTileEntity>> flowers = null;
+    public static final List<Class<? extends SubTileEntity>> flowers = new ArrayList<>();
 
-	@SidedProxy(serverSide = "tk.zeitheron.botanicadds.proxy.CommonProxy", clientSide = "tk.zeitheron.botanicadds.proxy.ClientProxy")
-	public static CommonProxy proxy;
+    @SidedProxy(serverSide = "tk.zeitheron.botanicadds.proxy.CommonProxy", clientSide = "tk.zeitheron.botanicadds.proxy.ClientProxy")
+    public static CommonProxy proxy;
 
-	public static final Logger LOG = LogManager.getLogger(InfoBA.MOD_ID);
+    public static final Logger LOG = LogManager.getLogger(InfoBA.MOD_ID);
 
-	public static final CreativeTabs TAB = new CreativeTabs(InfoBA.MOD_ID)
-	{
-		@Override
-		public ItemStack createIcon()
-		{
-			return new ItemStack(BlocksBA.MANA_TESSERACT);
-		}
+    public static final CreativeTabs TAB = new CreativeTabs(InfoBA.MOD_ID)
+    {
+        @Override
+        public ItemStack createIcon()
+        {
+            return new ItemStack(BlocksBA.MANA_TESSERACT);
+        }
 
-		@Override
-		public void displayAllRelevantItems(NonNullList<ItemStack> items)
-		{
-			Item sf = Item.getItemFromBlock(ModBlocks.specialFlower);
-			for(Item item : Item.REGISTRY)
-			{
-				item.getSubItems(this, items);
-				if(item == sf)
-				{
-					for(Class<? extends SubTileEntity> cl : flowers)
-					{
-						Flower fl = cl.getDeclaredAnnotation(Flower.class);
-						String id = "ba_" + fl.value();
-						items.add(ItemBlockSpecialFlower.ofType(id));
-					}
-				}
-			}
-		}
-	};
+        @Override
+        public void displayAllRelevantItems(NonNullList<ItemStack> items)
+        {
+            Item sf = Item.getItemFromBlock(ModBlocks.specialFlower);
+            for (Item item : Item.REGISTRY)
+            {
+                item.getSubItems(this, items);
+                if (item == sf)
+                {
+                    for (Class<? extends SubTileEntity> cl : flowers)
+                    {
+                        Flower fl = cl.getDeclaredAnnotation(Flower.class);
+                        String id = "ba_" + fl.value();
+                        items.add(ItemBlockSpecialFlower.ofType(id));
+                    }
+                }
+            }
+        }
+    };
 
-	@EventHandler
-	public void preInit(FMLPreInitializationEvent e)
-	{
-		ReflectionUtil.setStaticFinalField(BotanicAdditions.class, "flowers", AnnotatedInstanceUtil.getTypes(e.getAsmData(), Flower.class, SubTileEntity.class));
-		SimpleRegistration.registerFieldBlocksFrom(BlocksBA.class, InfoBA.MOD_ID, TAB);
-		SimpleRegistration.registerFieldItemsFrom(ItemsBA.class, InfoBA.MOD_ID, TAB);
-		MinecraftForge.EVENT_BUS.register(proxy);
-		proxy.preInit();
-	}
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent e)
+    {
+        // 手动添加所有花
+        flowers.add(WitherFlower.class);
+        flowers.add(RainFlower.class);
+        flowers.add(SnowFlower.class);
+        flowers.add(LightningFlower.class);
+        LOG.info("Registered {} flowers", flowers.size());
 
-	@EventHandler
-	public void init(FMLInitializationEvent e)
-	{
-		proxy.init();
+        SimpleRegistration.registerFieldBlocksFrom(BlocksBA.class, InfoBA.MOD_ID, TAB);
+        SimpleRegistration.registerFieldItemsFrom(ItemsBA.class, InfoBA.MOD_ID, TAB);
+        MinecraftForge.EVENT_BUS.register(proxy);
+        proxy.preInit();
+    }
 
-		GaiaPlateRecipesBA.init();
-		PetalRecipesBA.init();
-		AltarRecipesBA.init();
-		PoolRecipesBA.init();
-		ElvenTradesBA.init();
-		RecipesBA.init();
-		LexiconBA.init();
+    @EventHandler
+    public void init(FMLInitializationEvent e)
+    {
+        proxy.init();
 
-		ICTCompat ictc = CraftTweakerCompat.compat();
-		if(ictc != null) ictc.init();
-	}
+        GaiaPlateRecipesBA.init();
+        PetalRecipesBA.init();
+        AltarRecipesBA.init();
+        PoolRecipesBA.init();
+        ElvenTradesBA.init();
+        RecipesBA.init();
+        LexiconBA.init();
 
-	@EventHandler
-	public void loadComplete(FMLLoadCompleteEvent e)
-	{
-		ICTCompat ictc = CraftTweakerCompat.compat();
-		if(ictc != null) ictc.onLoadComplete();
-	}
+        ICTCompat ictc = CraftTweakerCompat.compat();
+        if (ictc != null) ictc.init();
+    }
 
-	@SubscribeEvent
-	public static void registerBlocks(RegistryEvent.Register<Block> e)
-	{
-		for(Class<? extends SubTileEntity> cl : flowers)
-		{
-			Flower fl = cl.getDeclaredAnnotation(Flower.class);
-			String id = "ba_" + fl.value();
-			BotaniaAPI.registerSubTile(id, cl);
-			BotaniaAPI.registerSubTileSignature(cl, new BasicSignature(id));
-			BotaniaAPI.addSubTileToCreativeMenu(id);
-		}
-	}
+    @EventHandler
+    public void loadComplete(FMLLoadCompleteEvent e)
+    {
+        ICTCompat ictc = CraftTweakerCompat.compat();
+        if (ictc != null) ictc.onLoadComplete();
+    }
 
-	@SubscribeEvent
-	public static void registerRecipes(RegistryEvent.Register<IRecipe> e)
-	{
-		ResourceLocation ultraSpreader = new ResourceLocation("botania", "spreader_3");
+    @SubscribeEvent
+    public static void registerBlocks(RegistryEvent.Register<Block> e)
+    {
+        for (Class<? extends SubTileEntity> cl : flowers)
+        {
+            Flower fl = cl.getDeclaredAnnotation(Flower.class);
+            String id = "ba_" + fl.value();
+            BotaniaAPI.registerSubTile(id, cl);
+            BotaniaAPI.registerSubTileSignature(cl, new BasicSignature(id));
+            BotaniaAPI.addSubTileToCreativeMenu(id);
+        }
+        LOG.info("Registered {} flower blocks", flowers.size());
+    }
 
-		ForgeRegistryUtils.deleteEntry(e.getRegistry(), ultraSpreader);
+    @SubscribeEvent
+    public static void registerRecipes(RegistryEvent.Register<IRecipe> e)
+    {
+        ResourceLocation ultraSpreader = new ResourceLocation("botania", "spreader_3");
 
-		ModContainer c = Loader.instance().activeModContainer();
+        ForgeRegistryUtils.deleteEntry(e.getRegistry(), ultraSpreader);
 
-		Loader.instance().setActiveModContainer(Loader.instance().getIndexedModList().get("botania"));
-		// Begin overrides
-		e.getRegistry().register(SimpleRegistration.parseShapedRecipe(new ItemStack(ModBlocks.spreader, 1, 3), "gsd", 'g', new ItemStack(ItemsBA.GAIA_SHARD), 's', new ItemStack(ModBlocks.spreader, 1, 2), 'd', LibOreDict.DRAGONSTONE).setRegistryName(ultraSpreader));
-		//End overrides
-		Loader.instance().setActiveModContainer(c);
-	}
+        ModContainer c = Loader.instance().activeModContainer();
+
+        Loader.instance().setActiveModContainer(Loader.instance().getIndexedModList().get("botania"));
+        // Begin overrides
+        e.getRegistry().register(SimpleRegistration.parseShapedRecipe(new ItemStack(ModBlocks.spreader, 1, 3), "gsd", 'g', new ItemStack(ItemsBA.GAIA_SHARD), 's', new ItemStack(ModBlocks.spreader, 1, 2), 'd', LibOreDict.DRAGONSTONE).setRegistryName(ultraSpreader));
+        //End overrides
+        Loader.instance().setActiveModContainer(c);
+    }
 }
